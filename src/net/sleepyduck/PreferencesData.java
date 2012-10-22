857d7a1684596ead75224f6319b791e6770aae53
Fredrik Metcalf <metcalf.fredrik@gmail.com>
2012-10-22 11:09:21 +0200

    Preferences Save/Load

--- src/net/sleepyduck/PreferencesData.java
+++ src/net/sleepyduck/PreferencesData.java
@@ -4,7 +4,21 @@
  */
 package net.sleepyduck;
 
+import java.awt.Component;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JTabbedPane;
+import nu.xom.Attribute;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+import nu.xom.ParsingException;
+import nu.xom.Serializer;
 
 /**
  *
@@ -29,8 +43,44 @@ public class PreferencesData {
 	}
 
 	public static void load() {
+		try {
+			File file = new File("preferences.ini");
+			if (file.exists()) {
+				Builder parser = new Builder();
+				FileInputStream fis = new FileInputStream(file);
+				Document doc = parser.build(fis);
+
+				Element root = doc.getRootElement();
+				CUFF_COLOR_SIZE = Integer.parseInt(root.getAttributeValue("CUFF_COLOR_SIZE"));
+				CUFF_COLOR_SIZE_EDIT = Integer.parseInt(root.getAttributeValue("CUFF_COLOR_SIZE_EDIT"));
+				CUFF_COLOR_GAP = Integer.parseInt(root.getAttributeValue("CUFF_COLOR_GAP"));
+				CUFF_EDGE = Integer.parseInt(root.getAttributeValue("CUFF_EDGE"));
+				PALETTE_BORDER_THICKNESS = Integer.parseInt(root.getAttributeValue("PALETTE_BORDER_THICKNESS"));
+				PALETTE_COLOR_SIZE = Integer.parseInt(root.getAttributeValue("PALETTE_COLOR_SIZE"));
+			}
+		} catch (ParsingException | IOException ex) {
+			Logger.getLogger(CuffMain.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 	public static void save() {
+			Element root = new Element("Preferences");
+			root.addAttribute(new Attribute("CUFF_COLOR_SIZE", "" + CUFF_COLOR_SIZE));
+			root.addAttribute(new Attribute("CUFF_COLOR_SIZE_EDIT", "" + CUFF_COLOR_SIZE_EDIT));
+			root.addAttribute(new Attribute("CUFF_COLOR_GAP", "" + CUFF_COLOR_GAP));
+			root.addAttribute(new Attribute("CUFF_EDGE", "" + CUFF_EDGE));
+			root.addAttribute(new Attribute("PALETTE_BORDER_THICKNESS", "" + PALETTE_BORDER_THICKNESS));
+			root.addAttribute(new Attribute("PALETTE_COLOR_SIZE", "" + PALETTE_COLOR_SIZE));
+			try {
+				Document doc = new Document(root);
+				File file = new File("preferences.ini");
+				file.createNewFile();
+				Serializer serializer = new Serializer(new FileOutputStream(file), "ISO-8859-1");
+				serializer.setIndent(4);
+				serializer.setMaxLength(64);
+				serializer.write(doc);
+			} catch (IOException ex) {
+				Logger.getLogger(CuffMain.class.getName()).log(Level.SEVERE, null, ex);
+			}
 	}
 }
